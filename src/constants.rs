bc267cb1cd8f137791fd3f5e2058cb76725797ce
Aaron Ang <aaron.angyd@gmail.com>
2025-04-25 12:47:18 -0700

    refactor: use clap, tokio, and anyhow

---
+++ src/constants.rs
@@ -0,0 +1,7 @@
+pub const PORT: u16 = 4221;
+
+pub const HTTP_OK: &str = "HTTP/1.1 200 OK\r\n";
+pub const HTTP_CREATED: &str = "HTTP/1.1 201 Created\r\n\r\n";
+pub const HTTP_BAD_REQUEST: &str = "HTTP/1.1 400 Bad Request\r\n\r\n";
+pub const HTTP_NOT_FOUND: &str = "HTTP/1.1 404 Not Found\r\n\r\n";
+pub const HTTP_INTERNAL_SERVER_ERROR: &str = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,84 @@
-use async_std::{
-    io::BufReader,
-    net::{TcpListener, TcpStream},
-    prelude::*,
-};
+use anyhow::Result;
+use clap::Parser;
 use flate2::{write::GzEncoder, Compression};
-use futures::{stream::StreamExt, AsyncBufReadExt};
-use httparse::{Header, Request};
-use std::{env, fs, io::Write};
+use httparse::{Header, Request, Status};
+use std::{io::Write, path::Path};
+use tokio::{
+    fs,
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
-static PORT: u16 = 4221;
+use http_server_starter_rust::*;
 
-#[async_std::main]
-async fn main() {
-    let directory = parse_args_for_directory();
-    let listener = TcpListener::bind(format!("127.0.0.1:{PORT}"))
-        .await
-        .unwrap();
+#[derive(Parser)]
+struct Cli {
+    /// Directory where the files are stored, as an absolute path
+    #[arg(long, default_value = ".")]
+    directory: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Cli::parse();
+    let directory = args.directory;
+    let listener = TcpListener::bind(format!("127.0.0.1:{PORT}")).await?;
     println!("Server listening on port {PORT}, using directory: {directory}");
 
-    listener
-        .incoming()
-        .for_each_concurrent(/* limit */ None, |tcpstream| {
-            let directory = directory.clone();
-            async move {
-                match tcpstream {
-                    Ok(tcpstream) => {
-                        handle_connection(tcpstream, directory)
-                            .await
-                            .unwrap_or_else(|e| {
-                                eprintln!("Failed to handle connection: {}", e);
-                            });
+    loop {
+        match listener.accept().await {
+            Ok((stream, _addr)) => {
+                let directory = directory.clone();
+                tokio::spawn(async move {
+                    if let Err(e) = handle_connection(stream, directory).await {
+                        eprintln!("Failed to handle connection: {}", e);
                     }
-                    Err(e) => {
-                        eprintln!("Failed to establish connection: {}", e);
-                    }
-                }
+                });
             }
-        })
-        .await;
+            Err(e) => {
+                eprintln!("Failed to accept connection: {}", e);
+            }
+        }
+    }
 }
 
-fn parse_args_for_directory() -> String {
-    let args: Vec<String> = env::args().collect();
-    let directory = if args.len() > 2 && args[1] == "--directory" {
-        args[2].clone()
-    } else {
-        ".".to_string()
-    };
-    directory
-}
-
-async fn handle_connection(mut stream: TcpStream, directory: String) -> Result<(), std::io::Error> {
+async fn handle_connection(mut stream: TcpStream, directory: String) -> Result<()> {
     let mut buf_reader = BufReader::new(&mut stream);
     let buf = buf_reader.fill_buf().await?;
+
     let mut headers = [httparse::EMPTY_HEADER; 16];
     let mut req = Request::new(&mut headers);
-    let res = req.parse(buf).unwrap();
-    let body = buf[res.unwrap()..].to_vec();
+    let res = req.parse(buf)?;
+    let body = match res {
+        Status::Complete(size) => buf[size..].to_vec(),
+        _ => vec![],
+    };
 
-    println!("Headers: {:#?}", req.headers);
-    println!("Body: {:#?}", String::from_utf8_lossy(&body));
+    println!("Received headers: {:?}", req.headers);
+    println!("Received body: {:?}", String::from_utf8_lossy(&body));
 
     let path = req.path;
     let response = match path {
-        None => "HTTP/1.1 400 Bad Request\r\n\r\n".to_string(),
+        None => HTTP_BAD_REQUEST.to_string(),
         Some(p) => {
             if p == "/" {
-                "HTTP/1.1 200 OK\r\n\r\n".to_string()
+                format!("{HTTP_OK}\r\n")
             } else if p.starts_with("/echo") {
                 match handle_echo_endpoint(p, req.headers) {
                     Ok(response) => response,
-                    Err(_) => "HTTP/1.1 500 Internal Server Error\r\n\r\n".to_string(),
+                    Err(_) => HTTP_INTERNAL_SERVER_ERROR.to_string(),
                 }
             } else if p.starts_with("/user-agent") {
                 handle_user_agent_endpoint(req.headers)
             } else if p.starts_with("/files") {
-                handle_files_endpoint(req, directory, body)
+                handle_files_endpoint(req, &directory, &body).await?
             } else {
-                "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+                HTTP_NOT_FOUND.to_string()
             }
         }
     };
+
+    println!("Response: {}", response);
 
     stream.write_all(response.as_bytes()).await?;
     stream.flush().await?;
@@ -96,65 +94,73 @@ fn find_header_value(headers: &[Header], key: &str) -> Option<String> {
     None
 }
 
-fn handle_echo_endpoint(path: &str, headers: &[Header]) -> Result<String, std::io::Error> {
-    let mut string = path.split('/').last().unwrap().to_string();
-    let mut response = "HTTP/1.1 200 OK\r\n".to_string();
-    let mut compressed: Option<Vec<u8>> = None;
+fn handle_echo_endpoint(path: &str, headers: &[Header]) -> Result<String> {
+    let mut payload = path.rsplit('/').next().unwrap().to_string();
+    let mut content_encoding = None;
 
     if let Some(encoding) = find_header_value(headers, "Accept-Encoding") {
         if encoding.contains("gzip") {
-            response.push_str("Content-Encoding: gzip\r\n");
+            content_encoding = Some("gzip");
             let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
-            encoder.write_all(string.as_bytes()).map_err(|e| e)?;
-            compressed = Some(encoder.finish().map_err(|e| e)?);
+            encoder.write_all(payload.as_bytes())?;
+            let compressed = encoder.finish()?;
+            payload = unsafe { String::from_utf8_unchecked(compressed) };
         }
     }
 
-    if let Some(c) = compressed {
-        unsafe { string = String::from_utf8_unchecked(c) }
-    }
-    response.push_str(&format!(
-        "Content-Type: text/plain\r\nContent-Length:{}\r\n\r\n{}",
-        string.len(),
-        string
-    ));
-    Ok(response)
+    Ok(build_response(
+        HTTP_OK,
+        content_encoding,
+        "text/plain",
+        &payload,
+    ))
 }
 
 fn handle_user_agent_endpoint(headers: &[Header]) -> String {
     match find_header_value(headers, "User-Agent") {
-        Some(user_agent) => format!(
-            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:{}\r\n\r\n{}",
-            user_agent.len(),
-            user_agent
-        ),
-        None => "HTTP/1.1 400 Bad Request\r\n\r\n".to_string(),
+        Some(user_agent) => build_response(HTTP_OK, None, "text/plain", &user_agent),
+        None => HTTP_BAD_REQUEST.to_string(),
     }
 }
 
-fn handle_files_endpoint(req: Request, directory: String, body: Vec<u8>) -> String {
+async fn handle_files_endpoint<'a>(
+    req: Request<'a, 'a>,
+    directory: &str,
+    body: &[u8],
+) -> Result<String> {
     let path = req.path.unwrap();
     let method = req.method.unwrap();
-    let file_name = path.split('/').last().unwrap();
-    let file_path = format!("{}/{}", directory, file_name);
-    let response: String;
+    let file_name = path.rsplit('/').next().unwrap();
+    let file_path = Path::new(directory).join(file_name);
 
-    if method == "POST" {
-        let res = fs::write(file_path, body);
-        response = match res {
-            Ok(_) => "HTTP/1.1 201 Created\r\n\r\n".to_string(),
-            Err(_) => "HTTP/1.1 500 Internal Server Error\r\n\r\n".to_string(),
-        };
+    let response = if method == "POST" {
+        match fs::write(file_path, body).await {
+            Ok(_) => HTTP_CREATED.to_string(),
+            Err(_) => HTTP_INTERNAL_SERVER_ERROR.to_string(),
+        }
     } else {
-        let file_content = fs::read_to_string(file_path);
-        response = match file_content {
-            Ok(content) => format!(
-                "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length:{}\r\n\r\n{}",
-                content.len(),
-                content
-            ),
-            Err(_) => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
-        };
-    }
-    response
+        match fs::read_to_string(file_path).await {
+            Ok(content) => build_response(HTTP_OK, None, "application/octet-stream", &content),
+            Err(_) => HTTP_NOT_FOUND.to_string(),
+        }
+    };
+    Ok(response)
+}
+
+fn build_response(
+    status: &str,
+    content_encoding: Option<&str>,
+    content_type: &str,
+    body: &str,
+) -> String {
+    let content_encoding_header = if let Some(encoding) = content_encoding {
+        format!("Content-Encoding: {}\r\n", encoding)
+    } else {
+        String::new()
+    };
+    format!(
+        "{status}{content_encoding_header}Content-Type: {content_type}\r\nContent-Length:{}\r\n\r\n{}",
+        body.len(),
+        body,
+    )
 }
